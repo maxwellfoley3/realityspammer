65081b329cfa6cfb38dbdae324745cf84b882714
Maxwell Foley <maxwell.foley@certik.org>
2023-09-04 21:02:59 +0100

    surrealism sketches

---
+++ src/pages/surrealism-sketches.tsx
@@ -0,0 +1,82 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import "./index.scss"
+
+const WhatIsAiHarmony: React.FC<PageProps> = () => {
+  return (
+		<div className="container-outer dark">
+    <div className="container dark">
+    <h1>Sketches towards the return of Surrealism</h1>
+		<h2>Surrealism in Itself</h2>
+		<p>
+		What is Surrealism? Surrealism is the movement dedicated to the idea that dreams and hallucinations and other aspects of the unconscious should enter waking life.
+		</p>
+		<p>
+		First and foremost, when we talk about returning to Surrealism, <b>we are not talking about an aesthetic</b>. The point is not to say, let’s start making paintings again that look specifically like Dali’s or Ernst’s or Miro’s. Rather, it is a return to a conceptual program. As aesthetic styles develop out of this conceptual program, there is no reason why they shouldn’t be original and contemporary and fresh.
+		</p>
+		<p>
+		Freud discovered two things — the existence of the unconscious, and the “talking cure”, which is the notion that by free-associating words in the presence of an analyst, one can eliminate neuroses and heal the self. The typical repressions which restrict the subject lift and unexpressed desires rush forth like a tide battering down a dam. However, psychoanalysis is based on the conceit that, once released, the chaoses of the unconscious can be wrangled back into a coherent subject again at the end of the session and she can go out in the world. The analyst must have her own slate of repressions to apply in order to facilitate this: ego, castration, Oedipus complex. Psychoanalysis has to try continuously harder to wrangle back in the cracks insanity it lets loose as a weapon against anxiety. Eventually, it can’t, which is why it’s impossible to see a psychoanalyst anymore these days.
+		</p>
+		<p>
+		Surrealism is just that the free association of the unconscious Freud discovered as a cure doesn’t need to happen in a therapist’s office — it should happen in the world, and not even just in writing and the art object — it should be a constant march of the dreaming unconscious down the street. The process of free association, of letting the unconscious sing, should go on as long as it can go.
+		</p>
+		<h2>Surrealism as a Lifestyle</h2>
+		<p>
+		What does the return to Surrealism primarily mean, as a lifestyle? <b>Return to the sensibilities of the early 20th century artistic avant-garde.</b> This is a call for those who want to imagine a different, more beautiful life, to mingle with each other in major cities and drink and smoke in cafes and establish creative conduits to shape the world’s artistic destiny.
+		</p>
+		<p>
+		We will return to the ethos of modernism and reject post-modernism — which was never an innovation in thought, but a mood of pessimism towards thought’s possibilities, inspired by economic conditions (according to eg Jameson). To be a modernist is simply to believe that it is possible for the mind to shape man.
+		</p>
+		<p>
+		We must seriously apply the idea that art forges man and society, and not believe that its purpose is limited to merely to gratifying audience or creator. But crucially, we must also rejecting the “activist” role of art, in which art is an emotional appeal to authorities to do something. Surrealism wants to intervene on a higher level by changing how we think and feel.
+		</p>
+		<p>
+		In our role, we have to cultivate a sort of aristocratic disdain for the trivial. We do not need to participate in “discourse”, we will probably not be understood. Dimes Square was entirely downstream from “discourse”, which is why it will not be remembered — because the artist should lead the masses, not the other way around.
+		</p>
+		<h2>Surrealism as Politics</h2>
+		<p>
+		Surrealism sees itself at the inheritor of Nietzsche’s thought, which is that to resolve civilizational problems, a group must come to see themselves as a new aristocracy who will create a new system of valuing and living in the world as a kind of great artwork, a task necessary due to the abandonment of the Christian God. The present return towards Surrealism is meant to be one path towards such a task.
+		</p>
+		<p>
+		Nietzsche is often abused and appropriated by racist and nationalist authors, who see this task to be carried out by an ethno-national group. But there is nothing Nietzsche despises more than this way of thinking, which he sees as herd-mindedness. Much of Nietzsche’s writing is a critique of the proto-National Socialist movements in his day, which he was so disgusted by that he invented a fake origin story for himself claiming to be Polish so that he would not be associated with “Germanness”. What Nietzsche was calling for was a cosmopolitan self-selected avant-garde.
+		</p>
+		<p>
+		The note <a href="on-national-socialism">“On National Socialism”</a> on realboy.fr argues that all capitalist democracies exert a pull towards national socialism when they enter crisis, and the only alternative path we can take is Surrealism in the political sense. This means not accepting capitalism, the nation-state, and so on as intrinsically “real”, and not logics that have to be taken seriously or worked within. A Surrealist must have the belief that new possibilities can emerge out of nowhere, as if by a miracle. For a Surrealist to deign to vote would be an odd personal choice. We will eventually create our own logics, new logics, as we participate in the basic Surrealist activity of letting our dreams unravel into the world.
+		</p>
+		<h2>Surrealism as Economy</h2>
+		<p>
+		Surrealism includes an economic theory. The greatest way in which Surrealism went wrong historically was through Andre Breton’s exercise of strict control over doctrine and activities, especially and specifically by enforcing Surrealist allegiance to Marxism-Leninism, which prevented Surrealism into developing into a potentially more radical and profound vehicle for change than Marxism-Leninism ever could be. One of the visionary figures Breton exiled was Antonin Artaud, who called Marxism “a lazy man’s revolution” and called for “a more essential change to society” — to pick one example.
+		</p>
+		<p>
+		Divine economy would also be a truer analysis of how capitalist economy functions, given that the dominant ideological description of how capitalism functions involves lies and dissimulation. An understanding of divine economy would allow someone to navigate capitalist economy without sacrificing faith and expression and virtue (or at least make those sacrifices or decisions more easy to understand). This is why we have begun to imagine the practice of Corporate Surrealism to describe how Surrealists can use divine economy to make money collectively, while simultaneously pursuing expression.
+		</p>
+		<h2>Surrealism as Epistemology</h2>
+		<p>
+		Surrealism is capable of discussing or exploring many things — alchemy, astrology,  angels, astrophysics, architecture, anthropology, analysis — without ever definitively embracing any ideas or paradigms as the Reality of the world. We are not interested in finding an answer to the question of what is real and what is not. But this is not because we are uninterested. On the contrary, only because we expect to continuously ask it, and never see it resolved. 
+		</p>
+		<p>
+		The Surrealist experience of the world is that it contains an impenetrable element of mystery, and that the further and more boldly one dives into this mystery, the more it blossoms more fully into a mystery beyond mystery, a mystery which only ever gets richer and deeper. The basic nature of ourselves, our perceptions, our facts about our lives, our memories, our dreams only become more and more dissolved into an ocean of implacability. All we know is that we are swimming farther and farther out, and getting less and less afraid of drowning.
+		</p>
+		<p>
+		Realists are like conceptual rapists who want to tear off reality’s dresses and violate her, and take what she rightfully possesses. They believe they have done this, but really she has trapped them in a gooncave with sex dolls, they thrust back and forth but bring forward no life, touch nothing. The world is adorned only in veils which when brushed aside reveal more veils in the form of flowers. 
+		</p>
+		<h2>
+		Surrealism as Spirituality 
+		</h2>
+		<p>
+		Surrealists believe in God, love, magic, and redemption.
+		</p>
+		<h2>
+		Surrealism and Technology
+		</h2>
+		<p>[To be written later]</p>
+    </div>
+		</div>
+  )
+}
+
+export const Head: HeadFC = () => <title>What is AI Harmony? And where to begin?
+</title>
+
+export default WhatIsAiHarmony
+
