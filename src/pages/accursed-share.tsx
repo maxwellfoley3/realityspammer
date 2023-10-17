b20369e9472b0c1f34175ba0246eaa5053333325
Maxwell Foley <maxwell.foley@certik.org>
2023-10-17 12:40:20 -0600

    heading change

--- src/pages/accursed-share.tsx
+++ src/pages/accursed-share.tsx
@@ -13,9 +13,9 @@ const CSVSPage: React.FC<PageProps> = () => {
 		<p>
 			(This is all from memory, written a day after finishing a first read which was more than a skim and less than a fully thorough penetration.)
 		</p>
-		<h4>
+		<h2>
 			Introduction
-		</h4>
+		</h2>
 		<p>
 		The goal of Accursed Share is to posit a new theory of “general economy”, which is distinguished from standard political economy in that political economy has heretofore only looked at things from the perspective of accumulation or growth, and ignoring the other side of consumption or waste, which Bataille sees as not only essential to consider, but the realm of the sacred, the divine.
 		</p>
@@ -34,16 +34,16 @@ const CSVSPage: React.FC<PageProps> = () => {
 		From here, Bataille presents several chronologically ordered case studies of general economy.
 
 </p>
-		<h4>
+		<h2>
 		Mexican civilization 
-		</h4>
+		</h2>
 		<p>
 		Bataille writes about the enormous carnivals of mass human sacrifice which would happen in Aztec civilization, during which thousands of people would be slaughtered yearly atop pyramids as offerings to the gods, as blood would stream in rivers down the steps and executioner-priests would wear the skin of the victims as cloaks while preparing the next one. He describes how the Aztecs conceived of the nobility of sacrifice through a primordial myth in which the greatest of the gods was the one who offered to throw himself into the sun, and thus became the sun.
 </p>
 		<p>
 		Bataille argues that despite being violent, Aztec civilization was prevented from ever becoming a “military”, imperial civilization, ie never developed a military bureaucracy or consistent war ethic, because it squandered its excess manpower in this way. If it had not found a way to sacrifice, the excess of energy would have overflowed into a war machine in this alternative manner.
 </p>
-<h4>Potlatch</h4>
+<h2>Potlatch</h2>
 		<p>
 		In early forms of society prior to large civilizational states in many areas, warring tribal chieftains and clans would demonstrate their superiority by their ability to waste, often by giving gifts to one another, but in certain extreme cases there was pure waste, including a practice iirc among certain Native Americans where if a warrior chieftain came face to face with a rival he would announce his presence by cutting the throat of one of his slaves in the view of his rival.
 
@@ -54,21 +54,21 @@ Bataille writes that, in gift-giving, one loses resources from the perspective o
 <p>
 At the end of this chapter, Bataille writes that the most noble life is that of the loafer who utterly scorns money and makes a ridiculous waste of his life, because he is able to reveal the “lie” that wealth is. Wealth to Bataille (including the spiritual wealth of “rank”? Need to re read) is a lie insofar as it demands to eventually be spent and sacrificed, and reveals its truth only in the way it is consumed.
 </p>
-<h4>Islamic civilization</h4>
+<h2>Islamic civilization</h2>
 <p>
 Bataille, in seeming contrast to the Mexican civilization described above, illustrates Islam as a situation in which the excess of society is turned into a disciplined war bureaucracy for conquest. He writes that we could perhaps just as easily cite the Roman Empire as the case study for a warlike society and the Christian Church for the subsequent study of a society of peace and renunciation, but that picking unfamiliar non-Western examples can illuminate the dynamics better for the reader.
 </p>
 <p>
 Bataille says that there is a paradox from the perspective of the Western aesthetic observer, which is that we observe a lot of beauty and poetry and love of life in Islamic civilization, but it is hard to discover how this attitude could have derived from the Quran, with its attitude of severity and total submission to God. Bataille argues that Arab civilization has a tendency towards pleasure and opulence and frivolousness, and it was the achievement of Mohammad to take all of the slack out of the Arab’s way of life and refocus the energy of the Arab men towards jihad. Once the jihad was fully accomplished, in the sense that the Arabs had built an empire and there was nowhere left to conquer, the slack inevitably went back into the system and the ways of the lives of the men returned partially to their original free state, leading to the current paradox in which we see a natural poetic Arab sensibility and the severity of Islam seemingly superimposed.
 </p>
-<h4>Lamaism</h4>
+<h2>Lamaism</h2>
 <p>
 In contrast to Islam, we are presented with the Tibetan society, which is unique for having never gone to war. Bataille explores a lengthy analysis of the geopolitics of Tibet and its diplomatic relations with Britain and China which allowed it to remain free from invasion while not keeping an army. However, due to needing to retain these relations under the wings of major powers, Tibetan sovereignty has always been somewhat illusory, with their affairs mostly governed by external powers and their kings limited to playing a sacred ceremonial role.
 </p>
 <p>
 Though the geographic isolation seemingly ensured that Tibet could never become a military power, it was only able to remain peaceful through squandering its excess in enormous amount of resources allocated to the church, including celibate clergy living opulent lives fueled by the labor of Tibetan peasants. When the thirteenth Dalai Lama in the early 20th century attempted to work with British Orientalists sympathetic to Tibet to figure out how to transform Tibetan society into one which was capable of exerting actual sovereignty, Tibet experienced mass protest by the monks as well as the populace, who loved the monks, because this would mean a shift of resources from the church into war and infrastructure, which would mean the loss of the uniquely church-centric Tibetan way of life.
 </p>
-<h4>Calvinism</h4>
+<h2>Calvinism</h2>
 <p>
 Bataille repeats Max Weber’s analysis of the origin of capitalism in the Protestant work ethic. Bataille seems far more sympathetic to Catholic society and views the Protestant reformation as a sort of abandonment of God from the earth. Under Catholicism, there would be regular sacrifices of wealth in the form of building cathedrals and funding the lives of the clergy. But in Protestantism, all wealth is reinvested for productivity, leading to a dangerous situation from the perspective of general economy. “You know white people, get money, don’t spend it”.
 </p>
@@ -78,11 +78,11 @@ Here we get to Bataille’s notion of how “intimacy” ties into his concept o
 <p>
 Bataille blames Calvin over Luther as specifically being “the Mohammed of the bourgeois”, in that he gave them a relationship to God which removed all the slack from their lives and turned them into a war machine to conquer the earth. The new Calvinist relationship with God is an entirely interiorized one, which accepts the imperative, the duty, of constant production as a requirement for entering the next world, and as such, God is evacuated out of this earth and there are no more cathedrals.
 </p>
-<h4>The bourgeois world</h4>
+<h2>The bourgeois world</h2>
 <p>
 The bourgeois world is fundamentally self contradictory, because it is characterized by a rejection of luxury, following the Protestant work ethic, but also in actual fact, the presence of luxury. Thus the bourgeois is a confused person with an incoherent consciousness. It is perhaps inevitable that this consciousness be succeeded by something like the clearer Marxist one, in which luxury is fully abandoned and the bourgeois casts off or is forced to cast off their implicit claim to a luxurious life.
 </p>
-<h4>The Soviet Union</h4>
+<h2>The Soviet Union</h2>
 <p>
 Bataille characterizes Marxism as an intensification of the logic of capitalism, or production, or the logic of “things”, rather than its rejection. Marx, who is delicately mystical at times, seems to imagine that if the logic of production is carried out to its full intensity, it will allow for the easy accumulation of enough resources that man will be able to spend more time idle than he ever has before. Thus, a sphere of the “full human”, once in which “intimacy” is restored, can potentially be created by stripping the sphere of production of everything that is external to it. This attitude would represent the intensification of the Calvinist submission to the logic of production and rejection of luxury that the bourgeois, in their confused consciousness, is unwilling to do.
 </p>
@@ -95,11 +95,11 @@ Politically, Bataille seems sympathetic to the ideal of socialism prior to Lenin
 <p>
 That being said, Bataille presents an unusual defense of Stalin. As with Islamic civilization, Stalin has turned the East into a massive war machine, through a rapid buildup of industrial production. But Bataille argues that this was in retrospect necessary to do, because Russia rapidly went from being an undeveloped “backwards” country to successfully defending itself at Stalingrad from genocidal total warfare. Stalin saw the existential need for Russia to defend itself coming, and also knew that to build up a war machine in this service would require a sacrifice from each person that no one would be talked into freely, thus terror and oppression had to be made the order. Bataille argues that after the awkward outcome of revolutionary socialism only being established in one country, and specifically an agrarian one lacking a war industry, there would have been no possible way for the revolution to defend itself other than Stalinism, and thus Stalin is a loyal knight of the revolution, rather than its betrayer according to the Trotskyite narrative. 
 </p>
-<h4>The Marshall Plan</h4>
+<h2>The Marshall Plan</h2>
 <p>
 Despite knowing that this makes him a “madman”, Bataille conceives of the Marshall Plan as a solution to re-arming Europe against Stalin’s military might as a kind of divine revelation (he says “apotheosis”). This is because the capitalist powers have chosen to make a break from the logic of only investing in more production, in order to “invest in the world” through a gift, or a sacrifice. Bataille seems to think that through this decision, the laws of general economy will finally be fully known to the world as a self-conscious framework for decision-making. (It’s unclear to me exactly how this thesis may or may not need to be revised in light of the subsequent events of the Cold War.)
 </p>
-<h4>Bataille’s moral and spiritual position</h4>
+<h2>Bataille’s moral and spiritual position</h2>
 <p>
 Bataille is a figure who invites a lot of speculation with regards to what his positive ambition for society actually is. Many seem to read him as a pervert who is interested in destruction, chaos, criminality for its own sake. Certainly he has moments which invite the reading of him as a Satanist, such as in <i>Story of an Eye</i> when the protagonists rape and murder a Catholic priest on an altar, or his founding of a secret society around the concept of human sacrifice.
 </p>
@@ -115,7 +115,7 @@ Gnostic Christianity has been widely slandered today by polemicists such as Voeg
 <p>
 A lot of the debate about what the essence of Gnosticism is reflects an irritating factor with debates around “isms” in general, is that in my experience these anxieties over “isms” are usually entertained by people who “weren’t there, and won’t get it”. People who read texts without participating in sociopolitics directly often feel compelled to internally debate and distinguish between a bunch of fixed “isms” they are receiving secondhand. Whereas people directly involved in sociopolitical life speak of cliques, vibes, individual people etc which are constantly in flux. Similarly, the essence of gnostic Christianity is a claim to have directly experienced revelation — gnostic means “one who knows”. Nor is its history limited to a single revelation, but rather a continuous stream of revelations establishing various sects until its lineage would be extinguished through its persecution by the Roman church. So it does not seem to me like the legacy of gnostic Christianity could be evaluated absent an investigation of each moment of revelation and the various zones of fallout which resulted in each instance, but unfortunately most of this history is now lost.
 </p>
-<h4>Roadmap for further investigations into Bataille</h4>
+<h2>Roadmap for further investigations into Bataille</h2>
 <p>
 I intend to read most of Bataille’s work, it seems to me like he might be a very crucial figure to build from. I can think of a number of arguments in favor of Bataille being a tentative candidate for figurehead at the helm of a conceptual vehicle which for now might be called “Surrealism NYC”.
 </p>
