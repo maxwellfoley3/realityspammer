f928649b6b7e9be07719112b7a6e0da56c3ccc70
Maxwell Foley <maxwell.foley@certik.org>
2023-10-23 00:15:54 -0400

    divine economy

---
+++ src/pages/divine-economy.tsx
@@ -0,0 +1,69 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import "./index.scss"
+
+const CSVSPage: React.FC<PageProps> = () => {
+  return (
+    <div className="container">
+    <h1>Towards Divine Economy</h1>
+		<div><a href="/">← Home</a></div>
+		<p>
+		<i>“Loss and gain is the same”</i> — Drain Gang
+		</p>
+		<p>
+		In our dyad, Corporate Surrealism is the groom and Divine Economy is the bride. Corporate Surrealism is our business practice, the structure of a new type of corporation, a different, unrealistic kind. Divine Economy is the nonstructured, the ineffable, the flux of all things that the surrealist must know how to tap into, but can never claim to fully possess or know. Divine Economy is water. Divine Economy is the question of everything under the sun. Divine Economy can only ever be a poetic and can never become a science.
+		</p>
+		<p>
+		What is the question of Divine Economy? The question of how one accumulates wealth. Several forms of wealth need to be considered in their convertibility to establish a flat monism: material resources, immaterial financial holdings, health, attention, power and influence, spiritual fruition, and the psychological charge within semiotics.
+s		</p>
+		<p>
+		Divine Economy is a Nietzschean economy. It is certainly true that capitalists are slaves, slaves to the realist abstraction that demands they accumulate interest on their capital over and over rather than satisfying their actual desires. The only capitalists that are not like this are those such as Kanye West or Elon Musk who routinely lose enormous fortunes and gain it back out of service to a higher aim than mere financial wealth.
+		</p>
+		<p>
+		Divine Economy in its mature form would look like a language, a new poetic or semiotic that fluently translates between multiple registers of the heretofore-cleaven planes of the divine economy. This semiotic would, self-conscious of the currency within semiotics itself, contain an Ourobouric logic that ensures its infinite self-possession, its royalty.
+</p>
+		<p>
+		Continuing with the earlier theme — Bataille writes that it is relatively unproblematic for any system to build up of energetic resources within it, but must make a key decision of how to dispel its excess, which is where the question of defining the sacred (the sacrificial) comes in. If the society cannot make a good decision of how to squander its excess, the excess will overflow into violence or wars of conquest. (The potential dialogue between Bataille and Malthus on population carrying capacities in an ecosystem and how that intersects with political economy is interesting to consider.)
+		</p>
+		<p>
+		Corporate Surrealism cannot on its own guarantee the type of universal redemption that Marxism, or even neoliberalism promises, as under Corporate Surrealism only those within the corporation develop wealth. Divine Economy may or may not, in the end, develop a framework for universal redemption. But this is not known yet.
+	</p>
+		<p>
+		Some points of investigation to begin with
+</p>
+<p>
+● The extraction of Guattari’s ontology from <i>Anti-Oedipus</i> which flattens psychology and economy into a single plane — but in a format that makes it into a workable conceptual toolkit, which has not been done yet
+</p>
+<p>
+● The economic reading of the Gospels, “redemption” being economic language  for instance, or the potential equivalence which can be made between the concepts of guilt, sin, and debt. (This seems like an unfortunate omission from <i>Anti-Oedipus</i>, which brushes the Christian conceptual innovation off as marginal, but Graeber’s <i>Debt</i> seems to integrate it in where it’s missing.)
+</p>
+<p>
+● Revolutionary history as read through Divine Economy: the gospels to Blake to Bataille to Guattari to Graeber to Occupy Wall Street as most recent possibility of mass resistance to technocapitalism. Under this framework, the specter of socialism has, at least in America, moved from Marxist frameworks and towards a sort of ecological libertarianism without adjectives. The collapse of Occupy and the splintering of the dialectic into wokeness vs neoreaction can be read as an entirely negative act of sabotage, or as the move of the demand for redemption into the psychic plane, as wokeness histrionically demands acknowledgement of historic wrongs before any utopian vehicle can be formed.
+</p>
+<p>
+● Nick Land style analysis and prose in which we perceive economic and technic structures as emerging from thoughts in a vast nonhuman mind — however one which remains critical and rejects his maneuver which imagines Capital to be a transcendent entity which exists outside of time.
+</p>
+<p>
+● A new form of socialist realist literature which investigates the psychic structures of PMC white collar workers necessary to sustain technocapitalism, and thus the convertibility of psychic trauma into capital accumulation at its most advanced level
+</p>
+<p>
+● A more detailed analysis of the incentives of attention economy within various social media platforms, and the forms of power and popular sovereignty that is established as a result
+</p>
+<p>
+● A history of art that involves exploring the way value is enshrined in an art object through critical lenses like Benjamin and Adorno
+</p>
+<p>
+● The hyperstitional praxis of cryptocurrency pumping, the practice of conveying memetic capital into a new currency, including in more elaborate forms like DAOs
+</p>
+<p>
+● Research into or experiment with practices of non rational and acausal means of accruing wealth, such as psychic manifestation or divination
+</p>
+    </div>
+		
+  )
+}
+
+export const Head: HeadFC = () => <title>Towards Divine Economy</title>
+
+export default CSVSPage
+
