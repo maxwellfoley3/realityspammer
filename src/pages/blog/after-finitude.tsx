208daf05f31f90a85cdf01fa4ceefc8ad18cb14a
Maxwell Foley <maxwell.foley@certik.org>
2023-12-28 02:05:17 -0500

    after finitude

---
+++ src/pages/blog/after-finitude.tsx
@@ -0,0 +1,113 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import "../index.scss"
+
+const AfterFinitude: React.FC<PageProps> = () => {
+  return (
+    <div className="container">
+    <h1>“After Finitude” by Meillassoux</h1>
+		<div className="date"><i>December 28, 2023</i></div>
+		<div><a href="/">← Home</a></div>
+		<h2>Hyper-chaos</h2>
+		<p>
+		I recently read this philosophy book <i>After Finitude</i> by Quentin Meillassoux. The purpose of this book, as far as I can tell, although this is not wholly explicit, is to advance a kind of militant atheist standpoint into philosophy and rid it of its sites of potential allegiance with ideas and sensibilities derived from religion. Heidegger and Wittgenstein are identified as two reigning philosophers who have made pacts with religion, which Meillassoux doesn’t seem to approve of.
+		</p>
+		<p>
+		Common sense, most contemporary philosophy, sociopolitical norms, and the mystical currents of most world religions all agree on one thing: <i>God is mysterious</i>. We cannot claim to true and rigorous knowledge of the Absolute. An impenetrable veil exists which separates us from an understanding of our creator.
+		</p>
+		<p>
+		The “finitude” of the title, from my reading, refers to this veil, which separates us from the infinity of the Absolute. Our knowledge has heretofore been held to be finite, because we imagine we may not surpass this veil and know the Absolute. However, Meillassoux believes that we <i>can</i>. 
+		</p>
+		<p>
+		Meillassoux‘s text’s aim is to affirmatively prove that we can say what the Absolute underlying the creation of this universe is: it is a <i>hyper-chaos</i> from which anything and everything can and will happen. That is our one and only true God.
+		</p>
+		<p>
+		If you accept Meillassoux’s argument in this book, then it seems possible to definitively say that you can know that we live in a <i>Rick and Morty</i> style multiverse of potential insanity in which three-eyed frogs in Elvis costumes might descend from a portal in the sky tomorrow. Such is the hyper-chaos governing this world.
+		</p>
+		<p>
+		However, there is a specific limitation on the hyper-chaos. The hyper-chaos is not able to generate any contradictory elements. So it is not possible to say that we could even be so fortunate that an entity like the Christian God would have emerged <i>out of</i> this chaotic Absolute, because the Christian God is contradictory: it contains within itself the Son which is the same as the Father, yet also different. Its omnipotence also leads to a contradiction: it is capable of creating a rock so heavy not even it could lift it. This latter example illustrates the fact that, to Meillassoux, the prohibition against contradictory elements is intimately tied to a deeper prohibition against <i>eternal</i> elements (because if the hyper-chaos contained an element that existed for eternity, it would stop being hyper-chaotic). 
+		</p>
+		<p>
+		Thus, it is not just the Christian God, but all the objects of divinity contemplated by the lovers of the numinous around the world which must be said to be fictions — O Krishna, O radiant one, infinite well of compassion, wonder and light, which I long to dissolve into, in watery embrace. But to Meillassoux, no such infinite thing can be the object of our wonder and love — the only infinity is an infinite anarchy.
+		</p>
+		<h2>Against Kant</h2>
+		<p>
+		In <i>After Finitude</i>, three philosophical positions regarding the Absolute are contrasted: 
+		</p>
+		<p>
+			1. Dogmatic metaphysics, in which it is believed that claims about the structure of the Absolute can be discovered and argued through reason. This is the old world of theology, rational religion, and the like, now widely discredited as naive. 
+		</p>
+		<p>
+			2. Kant’s critical philosophy, and all philosophy that follows it. Kant famously proved that we are not able to know the Absolute — which he calls the noumena or the thing-in-itself. We are only able to meaningfully say things about the phenomena we experience, not what is “behind” them, generating them. Meillassoux calls Kant’s position “correlationism”, because Kant is saying that while we can certainly say that there is some kind of <i>correlation</i> between the objects of experience and the noumenal Absolute generating them, that’s about all we can say.
+		</p>
+		<p>
+			3. Meillassoux’s own position, which he doesn’t give a name to. This is, again, the position that we can know that the Absolute is a hyper-chaos. This is not the same as dogmatic metaphysics, according to Meillassoux, because it only makes a <i>single</i> claim and then retires its claim to knowledge. “One thing and only one thing is for sure: it’s all just a big old mess!”
+		</p>
+		<p>
+		It should be noted that the stated point of Kant’s philosophy is to make philosophy compatible with religious belief in the Christian God. It was thought, in the beginnings of the Enlightenment, that as philosophy progressed, reason would be able to find more and more definitive arguments for belief in God and morality. Instead, the skeptical, critical current in thought raced ahead of the kind that bolstered existing orthodoxy. The danger began to loom in men’s minds that the use of reason would destroy existing arguments for God as not backed by sufficient reason, and thus lead to widespread atheism. 
+		</p>
+		<p>
+		Kant’s attempt to save Christianity was to designate limits to the use of reason, in what is called the critical turn. Here, Kant sets up the veil, which the Absolute lies behind. We don’t reason about what is beyond it! But after that, in Kant’s second critique, he argues that to have morality, we should act as if the thing beyond it has (at least some of) the qualities of the Christian God. Religion is saved, because we are now, unconstrained by rational skepticism, <i>allowed to believe again</i> — the atheist can tell us no more that there is nothing behind the veil than the theologian can tell us he has encountered it for certain. Let’s leave the matter at rest: who is to say about this mystery? It is unknowable, so it is for any of us to imagine on his own.
+		</p>
+		<p>
+		Meillassoux does not go so far as to argue that Kant’s position is provably false. He is instead arguing that <i>he has found an alternative position</i>, which is just as logically coherent. Therefore, Meillassoux has to argue that there are seductive reasons to embrace his own consistent position to Kant’s. The first is what we have been talking about this whole time: Kant’s position leaves too much room for religious faith — by positing a realm outside of where rational philosophy is able to go, we open up too much possibility for irrationality to rule. People of the most outlandish spiritual persuasions are allowed to talk about what their faith tells them is true about the Absolute, and we cannot tell them they are deluded. Philosophy has become too shy in what it is willing to argue against, and thus is now tolerating fanaticism.
+		</p>
+		<p>
+		The second reason Meillassoux gives in favor of his position is that he, unlike Kant, sees himself as able to make meaningful statements about events which occurred prior to the emergence of human consciousness. For example, we can know, scientifically, through carbon dating that the last dinosaur died about 65 million years ago. Kant’s position is that we can only say meaningful things about phenomena, and not things-in-themselves. But how can we say a meaningful statement about this event that no human was around to witness? 
+		</p>
+		<p>
+		For the Kantian, all we can do is make references to the phenomena: “We have received a signal from our instruments, such that it would appear to us, that the world is consistent with one in which a dinosaur died 65 million years ago”. But to say that we <i>know</i> it happened as a real event becomes an inadmissible leap. Thus, there is an odd sort of resonance with the skepticism of the Kantian on the one hand and the arguments of fundamentalist Christian creationists on the other, who claim Satan put the dinosaur bones in the ground and falsely dated their carbon to deceive and tempt faithful Christians — who should instead believe that the world only began at the same time as God created mankind, and has always only existed for us.
+		</p>
+		<p>
+		Meillassoux wants to move as far as we can away from this “human-centric” worldview that fundamentalist Christians (ironically) as well as some secularists live in. To the extent that we can, through science, make claims about things that happened a very long time ago or very far away, on immense or small levels, we should affirm their reality. We should understand ourselves, against the universe, as very, very small. The universe does not at base level consist of what is experienced by us — it consists of vast, cold structures of stars and dark matter, which though they elude our senses, are discernible as really real by mathematical instruments. It is very important to Meillassoux that we un-center ourselves into this wider cosmological world; we have no right to our myopia of the limited five senses anymore.
+		</p>
+		<h2>Meillassoux’s Argument</h2>
+		<p>
+		Meillassoux wants to prove the supreme existence of the hyper-chaos like this. Dogmatic metaphysics is the claim that some entity must necessarily exist: for example, the dogmatic metaphysician can argue that God must necessarily exist by the old arguments of theology; <a href="https://en.wikipedia.org/wiki/Unmoved_mover">prime mover</a>, <a href="https://iep.utm.edu/anselm-ontological-argument/">St. Anselm’s ontological argument</a>, etc. 
+		</p>
+		<p>
+		By contrast, positing the existence of the hyper-chaos is to claim that NO entity can necessarily exist (the hyper-chaos itself necessarily exists, but it’s not an “entity”, rather the supreme whirlpool from which within entities come and go).
+		</p>
+		<p>
+		The third, intermediate, position occupied by the Kantians, is to simply say that they cannot <i>prove</i> that any entity necessarily exists one way or the other. So in order to go from this position to his own, Meillassoux has to pressure the Kantians into moving from a claim about knowledge (“we cannot prove that any entity necessarily exists”) to a claim about reality (“it is certainly the case that no entity necessarily exists”). 
+		</p>
+		<p>
+		How can Meillassoux do this? He aims to demonstrate that the Kantian perspective actually implicitly presumes his own, because if the Kantian denies it, he will enter into contradictions.
+		</p>
+		<p>
+		What is this contradiction? If I’m reading Meillassoux right, he is (framed the shortest possible way) arguing that simply to imagine this impenetrable split between the phenomenon and the noumenon that the Kantian does — to set up the veil of knowledge separating us from the Absolute — you must also <i>assume</i> that there is no necessity in the noumenon, because if it was the case that there was, then you could derive it from the phenomenon. Thus, there it is; you have a positive form of knowledge after all: the knowledge that there is no necessity. Without seeing it, you have discovered your God.
+		</p>
+		<p>
+		From my reading, the main “way out” Meillassoux imagines that post-Kantian philosophy might proceed while avoiding his logical conclusion regarding hyper-chaos is to completely deny the reality of the noumenon — since we cannot know that it exists, or experience it, then there is nothing to say about it, not even that there is no necessity. If we could leap behind the veil, then there would only be another veil. From the perspective of this type of post-Kantian, the infinite series of veils is itself God — an unfolding mystery without bottom, inextricable from the reality of our experiencing it, asymptotically approaching a point of our discovery of the secret origin of creation which will never arrive. 
+		</p>
+		<p>
+		This is the path that post-Kantian philosophy did in fact largely take in its most famous forms, from Hegel most notoriously, to Schopenhauer, Nietzsche, and Deleuze. On this path, the unfolding experience of psychic life as it attempts to reach greater universals is <i>itself</i> defined as the Absolute, a cosmic mind that bridges God and man. But this conception of things is not acceptable to Meillassoux because, of course, it remains too solipsistic. To imagine this is the same as saying that nothing can exist outside of experience, which means that we can’t fully meaningfully say anything about many of natural science’s most far-reaching claims, such as the age of dinosaur fossils, or the earliest stars.
+		</p>
+		<h2>Thoughts & conclusions</h2>
+		<p>
+		I was reading this book along with some other texts in the “speculative realist”, Urbanomic-adjacent milieu, attempting to get a better sense of this strand of newer philosophy. I’m looking at Brassier’s <i>Nihil Unbound</i> right now, which goes deeper into this militant atheism by attempting to advance the stance of “nihilism” as much as possible (which isn’t an ethical or political stance to Brassier, it’s moreso embracing the idea that science has shown us that human life is meaningless because the universe is so big and the stars will eventually die out). Then, interestingly, there is the recently published <i>Pleromatica</i> by Gabriel Catren, in which Catren, in a break with these earlier authors, advocates for restoring an explicit affirmation of Christianity, in a limited role, in order to deal with the trauma of natural science’s displacement of man’s privileged role in the cosmos.
+		</p>
+		<p>
+		Honestly, I find the appeal of this milieu pretty limited so far. It seems to be in this awkward position of constantly trailing behind and catching up with the natural sciences, an inferior position it is all too well aware of. In its desire to construct philosophical systems for the sake of “assimilating” the far-reaching discoveries of science, it seems to lag behind vulgar popular conceptions of the world, like Reddit “<i>I fucking love science!</i>” space-rock atheism, rather than critiquing or molding or inspiring the mind. At its worst, this attitude is indistinguishable from e/acc’s strange mixture of enthusiasm and apathy: “technoscience is marching onwards and doing a lot of really crazy shit, and let’s just make sure we’re able to affirm it!”
+		</p>
+		<p>
+		Philosophers are correct I think to say that they exist in an awkward relation with the natural sciences, because the natural sciences are not especially in need of new philosophy to facilitate new discoveries, and it is difficult for philosophy to keep up. However, ethics, psychology, politics, law, social sciences, the arts, and people’s existential realities are still desperately in need of philosophy to provide guiding principles. Therefore I don’t really see the danger in an observer-centric conception of philosophy — in fact, a world where scientific claims are bracketed as being understood as the derivations of methods and epistemologies constructed by humans seems healthy to me.
+		</p>
+		<p>
+		Meillassoux of course is free to make the adventures in thought he believes are compelling; if he is more interested in following the natural sciences than examining ourselves, he may do that — his book is a fun and thought-provoking argument. But the thing that this whole milieu can’t really get away with in my opinion is: their adventures in thought are constantly accompanied with a sort of vocal excitement of having unlocked some radical new potential in thinking. Being able to think about the early period of the cosmos in these non-anthropocentric terms is portrayed as an exciting new voyage — Meillassoux proclaims that for the first time we can discover the “great outdoors”.
+		</p>
+		<p>
+		This kind of pathos, more prevalent in Brassier, takes positive glee in advancing “nihilism”. While nihilism may be of no use or even destructive to the general concerns of humanity, it is of great joy to the philosopher, who enjoys taking thought to untamed areas it boldly leads, away from the petty existential concerns of the masses. Brassier gets to think about outer space, at the expense of being a bastard of mankind. This philosophical pathos seems derived from Nick Land. But while Land’s renegade “schizophrenic” thought is veritably psychedelic for the reader — one reads Land and can begin to imagine that there are time-traveling demons haunting his computer — what such adventures does Brassier actually take you towards? Because you do not need philosophy to imagine that we are just a bunch of monkeys on a space rock with no privileged purpose — this is already a popular way of situating oneself. All philosophy is doing here, in terms of the imagination, is playing catch-up to the scientific picture by getting out from under the observer-centering legacy of Kant.
+		</p>
+		<p>
+		Atheism remains boring! If the appeal of thinking is to go on an imaginative adventure, then surely contemplative (quasi-)theism — whether of the old form of dogmatic metaphysics, or in its romantic post-Kantian varieties like Hegel — has the philosophy of the hyper-chaos beat any day. Meillassoux’s book is fun, but I haven’t seen yet that it leads anywhere further to play in. What is there for <i>thought</i> — as opposed to telescopes and probes — to discover further in the outermost reaches of space? There’s nothing there. Everything interesting and worthwhile seems to be gained from going deeper into ourselves.
+		</p>
+		<div><a href="/">← Home</a></div>
+    </div>
+  )
+}
+
+export const Head: HeadFC = () => <title>“After Finitude” by Meillassoux</title>
+
+export default AfterFinitude
+
