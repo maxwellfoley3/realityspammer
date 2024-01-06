ad1f6cb1191b5c2273b946be8ded335f0eed03f5
Maxwell Foley <maxwell.foley@certik.org>
2024-01-06 15:26:24 -0500

    all b converted to h3

--- src/pages/autology-manifest.tsx
+++ src/pages/autology-manifest.tsx
@@ -19,9 +19,7 @@ const CSVSPage: React.FC<PageProps> = () => {
 		Actually this would not be a science at all. It would be as Guattari calls it an “ethico-aesthetic paradigm”, because it would not presume to have the quality of scientific objectivity. It would be like a new language for describing the psychic world and guiding decisions.
 		</p>
 		<h2>Why existing frameworks are insufficient</h2>
-		<p>
-		<b>1. Standard Western therapeutic paradigm</b>
-		</p>
+		<h3>1. Standard Western therapeutic paradigm</h3>
 		<p>
 		This paradigm relies on a privileged observer subject who monitors the person's behavior like an object and uses heuristics to guide it towards a more desirable behavior. 
 		</p>
@@ -31,9 +29,7 @@ const CSVSPage: React.FC<PageProps> = () => {
 		<p>
 		Furthermore, one must adopt socially normative goals (stability, external markers of success, good relations with one's peers) as desirable
 		</p>
-		<p>
-			<b>2. Psychoanalysis</b>
-		</p>
+			<h3>2. Psychoanalysis</h3>
 		<p>
 		Psychoanalysis is inspiring because it is not grounded on treating the psyche as an object studied by an observer, but rather exploring it through an intersubjective relation discovered in language
 		</p>
@@ -49,29 +45,23 @@ const CSVSPage: React.FC<PageProps> = () => {
 		<p>
 		c - its canon is a huge mess of jargon which is very hard to find proper definitions for, like Freud never defines "ego". Every major figure has their own system which is spread throughout their work and various case studies. The confusing nature of the jargon leads to a lot of psychoanalytical discourse being pseudointellectual obscurantist pulp which is difficult to wade through (every system is going to have its body of b-tier literature that is just "hey, I can use this jargon too!" but in psychanalytic literature, this stuff is particularly bad)
 		</p>
-		<p>
-			<b>
+			<h3>
 				3. Deleuze & Guattari schizoanalysis
-			</b>
-		</p>
+			</h3>
 		<p>
 		This endeavor aims to take what is useful in psychoanalysis and rid it of its authoritarian aspects to become an analytical toolkit you can use yourself, or to examine society
 		</p>
 		<p>
 		However it is never presented in an ultimately usable form. <i>Anti-Oedipus</i> and <i>A Thousand Plateaus</i> each have completely different conceptual frameworks, both of which have major flaws. Guattari's final ontology would be presented in the book <i>Schizoanalytic Cartographies</i>, which is mostly unreadable. The investigation is pointing in the right direction but the result is found wanting.
 		</p>
-		<p>
-			<b>4. Spirituality and religion</b>
-		</p>
+			<h3>4. Spirituality and religion</h3>
 		<p>
 		Can be useful but is ultimately grounded on tradition and dogma, thus fails to be a vehicle for true psychological freedom.
 		</p>
 			<h2>Some factors of what is necessary</h2>
-		<p>
-			<b>
+			<h3>
 			Meta-decisional framework
-			</b>
-		</p>
+			</h3>
 		<p>
 		One must make an infinite amount of decisions each day just to live. What are the criteria used to make these decisions? One oscillates between various norms, modeling other people's perspectives in one's head -- "I'd better not bring up politics anymore today, people will think I'm annoying". In the big picture, one is making various life plans, but how to know if these plans are sound?
 		</p>
@@ -81,9 +71,7 @@ const CSVSPage: React.FC<PageProps> = () => {
 		<p>
 		But the ultimate orientation of a living thing is perhaps not "philosophical" per se -- it is maybe not to be determined by argument; rather discovered.
 		</p>
-		<p>
-			<b>Wall against psychosis</b>
-		</p>
+			<h3>Wall against psychosis</h3>
 		<p>
 		If you push for freedom too much in the psyche the result is psychosis, because you will find yourself outside of all structure.
 		</p>
@@ -93,7 +81,7 @@ const CSVSPage: React.FC<PageProps> = () => {
 		<p>
 		There must be a way to "construct one's own fortresses" that allows one to go on adventures into the unknown and return to sanity without introjecting social censorship. 
 		</p>
-			<h4>Auto-grounding</h4>
+			<h3>Auto-grounding</h3>
 		<p>
 		Autology cannot ground itself on another discipline. It exists within itself as one of its objects.
 		</p>
