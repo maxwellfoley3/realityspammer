3f246c35c6a18bac45ecea632c704fcd186ec549
Maxwell Foley <maxwell.foley@certik.org>
2024-01-20 18:50:50 -0500

    after optimism

---
+++ src/pages/after-optimism.tsx
@@ -0,0 +1,96 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import "./index.scss"
+
+const CSVSPage: React.FC<PageProps> = () => {
+  return (
+    <div className="container">
+    <h1>After Optimism</h1>
+		<div><a href="/">← Home</a></div>
+		<p>
+		Things seem pretty bad lately. Let’s look at what’s going on:
+		</p>
+		<p>
+		1. It seems likely that America will transition to an authoritarian right-wing system (“fascism” if you like), or in other terms experience the same “democratic backsliding” as Russia, India, Turkey, etc. 
+		</p>
+		<p>
+		2. The “end of history” idea of an era of global peace between democracies is pretty much a departed fantasy of the past. Russia experienced “democratic backsliding” and now is destroying Ukrainian cities in a conventional land war. Israel is committing genocide in Gaza with the seeming total approval of liberal world leaders. The looming crisis over Taiwan means that America will either have to fight some kind of war with China, or back down and cede Taiwan, in which case it would massively lose its authority as “world policeman” leading to more instability. War is seemingly erupting everywhere.
+		</p>
+		<p>
+		3. The arrival of artificial intelligence means that pretty much every white-collar career which might have guaranteed someone a stable comfortable life is now precarious, potentially automated away or destroyed by market forces. It seems like the only possible path left for a stable life is to accumulate substantial amounts of capital.
+		</p>
+		<p>
+		4. Anything you could call the “moral fabric of society” is pretty much eroded in America, through forces emerging from both the liberals and the right. For instance, the major moral victory of the past half-century was supposed to be the overcoming of racism, but now racism is aggressively returning: in a naked form from the right, but also the “woke” liberal insistence that all white people are guilty oppressors etc plays into this too.
+		</p>
+		<p>
+		5. The disastrous global response to Covid-19 makes it very difficult to trust that civilization can competently weather anything together, rather than becoming incoherent and depraved, set against itself.
+		</p>
+		<p>
+		6. On top of all this, it seems possible — some would say even probable — that humanity will go extinct in our lifetime, through artificial intelligence, climate change, manmade pandemics, or nuclear bombs.
+		</p>
+		<p>
+		One can nitpick how likely any of these points are in the specifics of them, or debate the degree to whether things are actually bad or not — “the economy is doing fine, you still have the highest standard of living of any civilization ever, there have always been things you could worry about, you’re coming to conclusions too quickly”, etc. And of course, one can always gesture towards possibilities of hope — the future is always uncertain — perhaps AI will not be destructive but instead usher in a new era of abundance where people will barely have to work, or some other unexpected good thing will happen.
+		</p>
+		<p>
+		The point at hand is not so much to complain about life — some will be able to enjoy themselves despite all this ongoing catastrophe, pockets of beauty may exist — but rather we must remark upon the collapse of a civilizational-level meta-narrative with nothing left to replace it in its absence. 
+		</p>
+		<p>
+		Since roughly the French Revolution, social and political life has been guided by the idea that the world will be made better through human will. This is the project of Enlightenment humanism. 
+		</p>
+		<p>
+		Both liberal and socialist ideology are variants of this. Each one posited a utopian “end of history” which would be achieved through economic expansion, culminating in universal peace through trade and human recognition on the one hand, or through  revolutionary consciousness on the other. Each of these dreams seems to have died at various stages, and attempts to cling to either of these visions as ideals today seem slightly archaic.
+		</p>
+		<p>
+		If it is both true that the projected global liberal world order is collapsing into a miasma of conflict — <i>and</i> that Western nations, particularly America, are moving away from liberal democracy — then we very much are exiting the epoch of civilization bound by the French Revolution. Integrating this development in history will require a shift in perspective unprecedented in centuries.
+		</p>
+		<p>
+		Liberals and right-wingers are both scrambling to find moments through which they can proclaim that America is “back” — Trump will “Make America Great Again” or Biden will “Build Back Better” by making the stock market go up or something and inaugurate a new era of optimism to get us out of our malaise. But it doesn’t actually seem like there is any way to escape the reality of this narrative collapse over the long arc — it’s not something that can be fixed with a new slate of policies and a determination to spread good vibes.
+		</p>
+		<p>
+		If things aren’t going to get better through human labor, then why do anything at all? One should only really bother living for easy satisfaction at this point. Certainly it would not make sense to sacrifice a lot for long-term projects — the so-called “high time-preference” which civilization is said to require. The arts, letters, and sciences (aside from those which can be made immediately profitable) all have an unclear justification, absent an ethic of progressive humanism.
+		</p>
+		<p>
+		How many people have moved on from an inner narrative of progress to an expectation of chaos? It is unclear because these things are not talked about. It’s not good to spread despair. Every time someone asks you “hi, how are you?” you say “doing well”. Even within online discourse, this sort of “doomer” “Mark Fisher capitalist realism” “dystopia” posting takes on a kind of flippant, memetic quality which makes its gravity unclear. Sometimes you will get it all at once on a Hinge date: the delicate premise in which it’s acceptable to rapidly escalate in intimacy with a total stranger will have you talking about how she doesn’t have pets but might get some to ten minutes later seeing this person breaking down in front of you about how the world is ending and she sees no hope for the future.
+		</p>
+		<p>
+		Maybe we should back up for a second. Is it really true that it is not possible to meaningfully live under conditions of decline, chaos, uncertainty, collapse? Have people not done this before? One could for instance point to Norse mythology in which (I’m going to repeat from memory) the world is being sustained by heroic gods fighting against terrible giants, the latter of which consistently take victories and are certain to eventually annihilate both the gods and the world. Pre-modern societies generally believe themselves to be living under situations of decline such as this — the Vedas tell the Hindus that we are living in the most fallen of the four <i>yugas</i>, just as Hesiod tells the Greeks that we are living in the last and worst of five successive ages.
+		</p>
+		<p>
+		There also happen to be a large group of people who have for a while believed that everything is getting worse and seem to be doing okay — conservatives. Or so one would imagine. In reality, in America, this is a bit counterintuitive, since even our conservatives are progressives of sorts (neoconservatism as a deviation from Trotskyite socialism), who also believe in the liberal metanarrative of an ideal society eventually being reached through industry, personal freedom, and global integration — they simply believe that some moral constructs and social distinctions need to be held in place to achieve this.
+		</p>
+		<p>
+		In theory, conservatives might also assert amongst themselves that it doesn’t really matter if everything is getting worse on a macro level because conservatives are supposed to take the ethic of personal responsibility — so as long as you can make sure things get better for you and your children you have a reason for being alive. I don’t really think that this logic works because people are deeply intertwined in the society around them in an inextricable fashion, and besides your children will maybe rebel against you and resent you in several ways. Actually, positing this is basically equivalent to saying that the meaning of life is to acquire capital, since this is the only way to secure a stable future for your descendants amidst general collapse.
+		</p>
+		<p>
+		There is this new weird logic amongst conservatives, like for example <a href="https://dailycaller.com/2023/12/26/tucker-carlson-one-true-objective-life-reproduction-kids-family/">I saw Tucker Carlson saying this on camera</a>. He was saying that the only objective meaning of life is to have children and spread your genes. Darwinism, essentially. This actually seems like a very odd and depraved form of nihilism to me. Ancient people who were bearing fruit and multiplying weren’t motivated to do this through any kind of knowledge of how genetics works. The kind of natural inclination of humans is to want to participate in something beyond oneself, and see it continue — and there are concentric circles here, so one is invested in the success of one’s family, then one’s local community, then the bigger picture of all things. To reduce the drive to create a world for your descendants to a base logic of if some genetic material reproduces strips life of all its meaning. I guess according to this logic some medieval monk who spent his life celibate painstakingly copying Latin texts on science so they would survive through the dark ages was a loser cuck who completely failed at life? What’s more is that according to this Darwinian logic, spreading your genes is only good to the extent that other people fail to so that you outcompete them. Tucker even acknowledges this; he says that some convicted felon out there who doesn’t know how condoms work might have seven kids he can barely provide for, so until you get ahead of him, he’s objectively beating you in the game of life. It all seems very depraved.
+		</p>
+		<p>
+		But what about people who are not mere conservatives, but are true reactionaries? Would this person not be actively celebrating the end of a two hundred year epoch bound by the concept of human progress? Would whatever order emerges after the end of progress — one of authority, violence, and struggle — not be preferable to the reactionary, who celebrates the emergence of such a thing?
+		</p>
+		<p>
+		Perhaps, but we have to ask the question of: what meta-narratives were there to inspire action and give hope prior to the belief in human progress? Obviously, in the West, it was belief in the Christian God. Of course under some kind of reactionary American authoritarianism there will be an attempt to re-seat faith in the Bible in public affairs — a specter of Christian Nationalism is looming with right-wing proposals such as <a href="https://www.project2025.org/">Project 2025</a>. 
+		</p>
+		<p>
+		But surely merely a kind of symbolic logic at the level of ideology is not enough to restore the happy faith of the medieval worker, who had no reason to doubt that the world was a structured order emanating from God’s splendor, as divinity and ritual then was woven into every aspect of social life. Imagining the world without God would have been hardly thinkable. 
+		</p>
+		<p>
+		Yet nothing like a patterned order sanctioned by and emanating from God is present today. A constant vicious flux continuously bombards all things, setting them into motion. Explosions of bizarre mimetic behavior, production of new and strange categories of goods, the splitting of the atom, odd types of pornographies and sexual fixations, biohacking, AI-generated art with seven-fingered hands, cryptocurrency, bass drops at EDM festivals and designer drugs, the omnipresence of technological interfaces and new categories of obsessive activity and mental illnesses to go along with them. The twin dancers of capitalism and techno-science promise us constant novelty to grapple with, and they do not disappoint. Everywhere, matter erupts in protest of and disobedience against the order-granting God.
+		</p>
+		<p>
+		What seems to me is needed is a new kind of religiosity, albeit one that does not stand against the novelty of contemporary science and technology yet is able to integrate it - or in other words is able to thoroughly suffuse the secular, rather than stand against it in paranoid defiance.
+		</p>
+		<p>
+		There will need to be a structure through which people can experience value in living in a way which orients them towards others, even though it doesn’t actually make sense to be oriented towards others; what makes sense is staying in your room on your phone finding new ways to make more money or get more attention. There will need to be a reason provided to strive heroically for various ends against the likelihood of success — or even remembrance, if it is possible for the world to wash away in the tide any moment. Commitment to humanist projects might need to come in the form of a sacred duty to preserve a flame against the wind, rather than a conviction in its fulfillment.
+		</p>
+		<p>
+		Creating this new understanding of the world and our role in it for the next epoch would seem to be the task of poets, artists, thinkers today.
+		</p>
+    </div>
+		
+  )
+}
+
+export const Head: HeadFC = () => <title>After Optimism</title>
+
+export default CSVSPage
+
