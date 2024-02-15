1d542b7971085cc3caabb665873624ee07125ded
Maxwell Foley <maxwell.foley@certik.org>
2024-02-15 13:23:35 -0500

    knockdown center review

---
+++ src/pages/blog/knockdown-center.tsx
@@ -0,0 +1,82 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import "../index.scss"
+
+const CSVSPage: React.FC<PageProps> = () => {
+  return (
+    <div className="container">
+    <h1>Tisakorean, 100 Gecs, and Liturgy</h1>
+		<div><a href="/">← Home</a></div>
+		<p>
+		On December 14, 2023 I saw <a href="https://pitchfork.com/news/100-gecs-tisakorean-and-liturgy-to-play-new-york-show-for-pitchfork-presents/">this concert hosted by Pitchfork Media at the Knockdown Center in Brooklyn</a> where the black metal band Liturgy, the rapper Tisakorean, and the hyperpop duo 100 Gecs performed. The concert was fun, the crowd was into it, and I enjoyed all three sets. 
+		</p>
+		<p>
+		The one thing I didn’t like about the Knockdown Center is that I bought a drink and this hot hipster girl who was bartending drew a heart shape on the receipt, so I thought she was flirting with me and felt flattered. But then I bought another drink from a different bartender, and she also drew a heart on the receipt — I guess that’s just their policy? What’s the point of this, just to mess with your head? Anyway, whatever.
+		</p>
+		<p>
+		The curation of this set of artists was fascinating to me — on the surface, none really have anything in common. But there is a way in which what I felt like I was being presented with were three absolute <i>theses</i> on the future of music today and where it could go.
+		</p>
+		<p>
+		Discourse around music today revolves around this question of “poptimism”. In the 00s, Pitchfork Media famously became a tastemaker by writing extremely snobbish appraisals of indie rock. Around 2013 or so, a new sensibility among critics began to form that music criticism was overvaluing indie rock music and undervaluing mainstream pop through its snobbery. This was accompanied by a “woke” critique — indie rock appeals to the perspective of white heterosexual men, while pop music is more interwoven with contributions from women, queers, non-whites. Undeniably there is some truth to this critique. The more cynical perspective on this, however, is that this is just an elaborate excuse for the media company doing the criticism to lower its standards and sell out by working with artists with a bigger budget — those who hold this view point to <a href="https://pitchfork.com/news/61621-pitchfork-acquired-by-conde-nast/">the 2015 acquisition of Pitchfork by Condé Nast</a>. Now, funnily enough, <a href="https://www.nytimes.com/2024/01/17/business/media/music-magazine-pitchfork-merger-gq.html">Condé Nast has recently sold Pitchfork to the men's magazine GQ</a> — is this a return to tradition, does this mean the taste of the white(?) man will reign once more?
+		</p>
+		<p>
+		The second question of music today is its overproduction. The optimism of pop can only come from its Warholian utopianism — the idea that we could maybe one day all be pop stars. This increasingly approaches a reality. The easy access to music production and distribution tools creates an oversaturation which is increasingly establishing a monotony in sound, as nothing is able to meaningfully stand out.
+		</p>
+		<p>
+		The three artists who performed at the Knockdown Center each can be read at presenting their own thesis on what the telos of music might be under these conditions.
+		</p>
+		<h3>Tisakorean</h3>
+		<p>
+		Tisakorean is a rapper from Houston who, to my knowledge, has pushed hip-hop farther to a point of minimalism, simplicity, repetition, than perhaps anyone before him. 
+		</p>
+		<p>
+		Even someone like Playboi Carti, noted hip-hop minimalist, has a kind of tonal complexity in the subtle sense of doomed romanticism he suggests and the textures of his instrumentals. None of this is there in Tisakorean, who hits a single note of bubblegum enthusiasm for the duration of his set.
+		</p>
+		<p>
+		All of Tisakorean’s lyrics are in this kind of register of “I’m wearing such-and-such outfit, I’m about to see what’s up with this girl, I think she thinks I look good, I’m about to do this dance”. Oh, and talking about being “silly”. He has a <a href="https://soundcloud.com/skrrt-lettuce/tisakorean-silly-bandana-intro">silly bandana</a>, <a href="https://www.youtube.com/watch?v=1IxYaOLtnqc">a silly shirt</a>, and is all-in-all <a href="https://www.youtube.com/watch?v=GkP9MI9mbjo">a silly dude</a>. 
+		</p>
+		<p>
+		Tisakorean’s music is perhaps a particularly intense expression of the method of the postmodern artist. The condition of postmodernity in art is that the artist is lifted from needing to have any self-conscious sense of his historical existence and relationship to the totality of things. Rather, he merely submits his art “un-self-consciously” (or in a silly fashion) to the world, and factors outside himself (the market) determine its ultimate relevance.
+		</p>
+		<p>
+		Tisakorean’s recent album is called “Let Me Update My Status”. Within his expressive register, everything he says takes on this kind of banal quality of a Facebook status — “hey, here I am, I’m wearing this article of clothing, what’s everyone up to tonight?” Perhaps this is what all artistic expression gets reduced to under Warholian over-production — the only meaningful thing one can say is “hi, here I am!” A new piece of art is just a notification that you’ve show up to the party. 
+		</p>
+		<h3>100 Gecs</h3>
+		<p>
+		If Tisakorean represents the pinnacle of what a single voice develops into under postmodern conditions of overproduction, 100 Gecs goes a step further, zooming-out by showing us what the *totality* looks like under such conditions.
+		</p>
+		<p>
+		All are invited to the party to yell their status update into a microphone. Culture becomes a panoply of expressions of basic impulses that barely cohere. The hyperpop style of 100 Gecs is to mash up all the manufactured, overproduced pop sounds one hears on the radio together into one unit in a grand expression of *bad taste.* Country, rap, EDM, screamo, all are placed side by side and allowed to wail in autotune about heartbreak and inferiority complexes.
+		</p>
+		<p>
+		100 Gecs’ music revels in its triviality, banality, and stupidity, even while attempting a kind of  ambitious maximalism. The goal of 100 Gecs’ music, it would seem, is to provide a reflection of the totality of production which the listener is thrust amongst in day-to-day life, now subsumed into a representative unity. 100 Gecs is a mirror to the total schizophrenia of pop culture at its limit of production — if one can no longer stand out amongst the cacaphony as an artist, one can hold a mirror to it and say look, I see you, this is what you are, and I choose to no longer resist.
+		</p>
+		<h3>Liturgy</h3>
+		<p>
+		Unlike these other two artists, a philosophical interpretation of Liturgy’s music is not a projection, as the band’s frontwoman Haela Hunt-Hendrix has written <a href="https://arkwork.org/">an extensive body of philosophical writings</a> which are woven into the band’s music. Liturgy’s philosophy has at times taken a variety of overlapping names such as “transcendental qabala”, “ark work”, and, most recently, “apocalyptic humanism”.
+		</p>
+		<p>
+		Liturgy’s music is a merging of the “extreme” genre of black metal with a kind of high Wagnerian operatic sensibility. The thesis of the band’s music, <a href="https://static1.squarespace.com/static/57044839b09f95d1f342ce25/t/575201a58a65e24600087b3b/1464992166180/Transcendental+Black+Metal+%28BMTS+vol.+1+-+Hideous+Gnosis%29+.pdf">outlined in manifestos</a>, is to take the popular form with the closest relationship to transcendence — held to be black metal, with its (sometimes authentically) spiritual relationship to the demonic, famously inspiring murder and church burnings — and taking the striving for transcendence further, to the point where it accesses the positively transformative and divine.
+		</p>
+		<p>
+		The utopian vision of apocalyptic humanism is one where each individual has the ability to maximize his potential for creative freedom, but not in a trivial sense in which he merely offers himself up as a body and belts out his immediate desire, as in a kind of silly Warholian factory. Instead, Liturgy advocates for a kind of classicist sensibility towards production in which discipline, rigor, studiousness play a role in the arts again. The idea is that art can actively make us better — more human, in a sense — both by producing and consuming it. 
+		</p>
+		<p>
+		One can perhaps dream of a restored culture that is truly a <i>culture</i>, in the sense of cultivating human souls, rather than an expression of what grabs people’s attention the quickest in a market. It would seem that Liturgy aims to save a world of overproduction by rejecting poptimism — we have to go beyond the shallow delirium of pop in favor of cultivation. Rather than the Warholian “everyone is a star”, the utopia to wish for is “everyone is a genius”.
+		</p>
+		<p>
+		The three theses of Tisakorean, 100 Gecs, and Liturgy make up a kind of Hegelian triad, analogous to the classic triad of the family, civil society, and the state. In the first case, we have a simple unity of the individual in his striving. In the second case, we have that form expressed in its negativity, in the totality of individuals in which coherence is no longer possible. In the third case, the individual takes the second case into awareness and uses it as the grounds to construct a new unity, thus allowing it to achieve a greater degree of absolute freedom.
+		</p>
+		<p>
+		These abstract positions correspond to the philosopho-political positions of neoliberalism, accelerationism, and whatever must come next.
+		</p>
+		<div><a href="/">← Home</a></div>
+    </div>
+		
+  )
+}
+
+export const Head: HeadFC = () => <title>Tisakorean, 100 Gecs, and Liturgy</title>
+
+export default CSVSPage
+
